e31de12e1b57a2a09bcb445a5cb67b49e359cc92
chenjian <sober1841@163.com>
2018-03-21 17:50:43 +0800

    This is my first test

---
+++ myspring/src/main/java/com/myspring/controller/MyController.java
@@ -0,0 +1,17 @@
+package com.myspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	@RequestMapping("/first")
+	public String getFirst(){
+		return "firstpage";
+	}
+	@RequestMapping("/second")
+	public String getSecond(){
+		return "secondpage";
+	}
+
+}
